8601737215ebbc88dd774689bad07d6c4c80452d
SebNik <lord.voldmeort1492@gmail.com>
2021-01-18 11:57:26 +0100

    working class

---
+++ 04-18-01-Klassen/song.rb
@@ -0,0 +1,28 @@
+# song.rb: Class song.
+#
+# Copyright (C) 2021 Niklas Abraham
+
+class Song
+
+    attr_accessor :title
+    attr_accessor :artist
+
+    def initialize
+        @title = "unknown"
+        @artist = "unknown"
+    end
+
+    def show_infos
+        puts "Song: #{@title} (#{@artist})"
+    end
+end
+
+
+# Klassendefinition an dieser Stelle ergaenzen
+
+song = Song.new
+song.show_infos  # Song: unknown (unknown)
+
+song.title  = "Revolution"
+song.artist = "The Beatles"
+song.show_infos  # Song: Revolution (The Beatles)
