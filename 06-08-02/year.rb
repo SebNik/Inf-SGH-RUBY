9e7c4ba8aed586df770b3225520e7685f7db7aa1
SebNik <lord.voldmeort1492@gmail.com>
2021-02-08 14:14:23 +0100

    first version of year

--- 06-08-02/year.rb
+++ 06-08-02/year.rb
@@ -4,5 +4,28 @@
 
 
 class Year
-    
+    def initialize(year)
+        @year_value = year
+    end
+
+    def leap?()
+        # regln für die Schaltjahre
+        # Ist die Jahreszahl durch vier teilbar, aber nicht durch 100, ist es ein Schaltjahr. 2008 fällt unter diese Regel.
+        # Ist die Jahreszahl durch 100 teilbar, aber nicht durch 400, ist es kein Schaltjahr. 2100 wird kein Schaltjahr sein.
+        # Ist die Jahreszahl durch 400 teilbar, dann ist es ein Schaltjahr. Deshalb war das Jahr 2000 ein Schaltjahr.
+        if @year_value%4==0 && (@year_value%100!=0 || @year_value%400==0)
+            true
+        else
+            false
+        end
+    end
+
+    def to_s()
+        @year_value.to_s()
+    end
+
 end
+
+year = Year.new(2001)
+puts year
+puts year.leap?
