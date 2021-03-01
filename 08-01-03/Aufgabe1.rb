4acaa0855a3168d98958a22f5a830c33261559a7
SebNik <lord.voldmeort1492@gmail.com>
2021-03-01 08:56:10 +0100

    first excersice and test

---
+++ 08-01-03/Aufgabe1.rb
@@ -0,0 +1,9 @@
+# Schreibe das Einstiegsbeispiel mithilfe einer while- oder until-Schleife um
+vegetables = ["potato", "cucumber", "tomato", "bean"]
+
+counter = 0
+
+while counter < vegetables.size
+    puts vegetables[counter].size
+    counter+=1
+end
---
+++ 08-01-03/test.rb
@@ -0,0 +1,14 @@
+number = 10
+while number <= 15
+    puts number
+    number = number + 1
+end
+
+
+
+answer = ""
+until answer == "y" || answer == "n"
+  print "Your answer [y/n]? "
+  answer = gets.chomp
+end
+puts "Your answer: #{answer}"
