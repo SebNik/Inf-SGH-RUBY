10e007145cecb71f3d73e5cf8bebc1665f885283
SebNik <lord.voldmeort1492@gmail.com>
2021-03-01 09:10:21 +0100

    added warming up exercise

---
+++ 08-01-03/Warming_Up.rb
@@ -0,0 +1,15 @@
+# Warming_Up.rb: Aufgabe von Warming Up
+#
+# Copyright (C) 2021 Niklas Abraham
+
+# Schreibe das Einstiegsbeispiel mithilfe eines each-Iterators um.
+# Verwende each, um zum Array vegetables von oben ein neues Array sizes zu erzeugen, das die jeweiligen Längen der Elemente enthält.
+
+vegetables = ["potato", "cucumber", "tomato", "bean"]
+length = []
+
+vegetables.each do |element|
+    puts element
+    length << element.size
+end
+puts length
