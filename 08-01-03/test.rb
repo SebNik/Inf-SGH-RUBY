aba9ac2901a9694e0362c6379e158a0a76fc03df
SebNik <lord.voldmeort1492@gmail.com>
2021-03-20 21:03:16 +0100

    refactored all of the code

--- 08-01-03/test.rb
+++ 08-01-03/test.rb
@@ -1,21 +1,16 @@
 number = 10
 while number <= 15
-    puts number
-    number = number + 1
+  puts number
+  number += 1
 end
 
-
-
-answer = ""
-until answer == "y" || answer == "n"
-  print "Your answer [y/n]? "
+answer = ''
+until ['y', 'n'].include?(answer)
+  print 'Your answer [y/n]? '
   answer = gets.chomp
 end
 puts "Your answer: #{answer}"
 
-
-
-
 10.upto(15) do |number|
-    puts number
+  puts number
 end
--- 08-01-03/zufallsnamen.rb
+++ 08-01-03/zufallsnamen.rb
@@ -4,47 +4,45 @@
 
 class NameGenerator
 
-    attr_reader :first_names
-    attr_reader :last_names
+  attr_reader :first_names
+  attr_reader :last_names
 
-    def initialize(f=nil, l=nil)
-        if f==nil && l==nil
-            @first_names = File.readlines("vornamen.txt", chomp: true)
-            @last_names = File.readlines("nachnamen.txt", chomp: true)
-        else
-            @first_names = f
-            @last_names = l
-        end
+  def initialize(f = nil, l = nil)
+    if f == nil && l == nil
+      @first_names = File.readlines('vornamen.txt', chomp: true)
+      @last_names = File.readlines('nachnamen.txt', chomp: true)
+    else
+      @first_names = f
+      @last_names = l
     end
+  end
 
-    def random
-        # this function will return an random name
-        @first_names.sample+" "+@last_names.sample
+  def random
+    # this function will return an random name
+    @first_names.sample + ' ' + @last_names.sample
+  end
+
+  def all
+    # this function returns out all the combinations for the names
+    combinations = []
+    @first_names.each do |first|
+      @last_names.each do |last|
+        combinations << "#{first} #{last}"
+      end
     end
+    combinations
+  end
 
-
-    def all
-        # this function returns out all the combinations for the names
-        combinations = []
-        @first_names.each do |first|
-            @last_names.each do |last|
-                combinations << "#{first} #{last}"
-            end
-        end
-        combinations
-    end
-
-
-    def show_all
-        # this function gives out all the combinations for the names
-        puts self.all()
-    end
+  def show_all
+    # this function gives out all the combinations for the names
+    puts all
+  end
 
 end
 
-#first_names = ["Big", "Little", "Flying", "Sitting"]
-#last_names  = ["Bull", "Eagle", "Fox", "Horse"]
-#names = NameGenerator.new(first_names, last_names)
+# first_names = ["Big", "Little", "Flying", "Sitting"]
+# last_names  = ["Bull", "Eagle", "Fox", "Horse"]
+# names = NameGenerator.new(first_names, last_names)
 
 # names = NameGenerator.new()
 
