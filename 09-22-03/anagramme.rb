3da9437c0e34c9c67dde0b31eb1610f56c6b7592
SebNik <lord.voldmeort1492@gmail.com>
2021-03-22 16:06:39 +0100

    refactored code fully

--- 09-22-03/anagramme.rb
+++ 09-22-03/anagramme.rb
@@ -1,29 +1,28 @@
-filename = "wortliste.txt"
-woerter = File.readlines(filename, chomp: true)
+# frozen_string_literal: true
+filename = 'wortliste.txt'
+words = File.readlines(filename, chomp: true)
 
-anagaramme_hash = {}
+anagrams_hash = {}
 
-woerter.each do |word|
-    signature = word.downcase.chars.sort.join
-    if (anagaramme_hash[signature])
-        anagaramme_hash[signature] << word
-    else
-        anagaramme_hash[signature] = [word]
-    end
+words.each do |word|
+  signature = word.downcase.chars.sort.join
+  if anagrams_hash[signature]
+    anagrams_hash[signature] << word
+  else
+    anagrams_hash[signature] = [word]
+  end
 end
 
-anagaramme_hash.each do |key, value|
-    if (value.size >= 2)
-        puts "Signature: #{key}, anagrams: #{value}"
-    end
+anagrams_hash.each do |key, value|
+  puts "Signature: #{key}, anagrams: #{value}" if value.size >= 2
 end
 
-max_key = ""
+max_key = ''
 max_anagrams = 0
-anagaramme_hash.each do |key, value|
-    if (value.size > max_anagrams)
-        max_anagrams=value.size
-        max_key=key
-    end
+anagrams_hash.each do |key, value|
+  if value.size > max_anagrams
+    max_anagrams = value.size
+    max_key = key
+  end
 end
-puts "longest anagram: #{max_key} is #{anagaramme_hash[max_key]}"
+puts "longest anagram: #{max_key} is #{anagrams_hash[max_key]}"
