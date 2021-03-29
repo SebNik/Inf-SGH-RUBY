a3678f28801d8e4d10499b0dd093c383bf6c416f
SebNik <lord.voldmeort1492@gmail.com>
2021-03-29 15:03:39 +0200

    put all of it in one big class

--- 10-29-03/mini_lexikon.rb
+++ 10-29-03/mini_lexikon.rb
@@ -4,31 +4,33 @@
 require 'csv'
 require_relative 'Country'
 
-filename = 'laender.csv'
-# data = File.readlines(filename, chomp: true)
-# # Code,Name,Name (Langform),Hauptstadt,ALPHA-3,TLD,Waehrung,Waehrungscode
 
-lexicon = {}
-
-# data.each do |row|
-CSV.foreach(filename, headers: true) do |row|
-  arr = row # .split(',')
-  c = Country.new(arr[0])
-  c.set_capital(arr[3])
-  c.set_name(arr[1])
-  lexicon[arr[1]] = c
-end
-
-puts lexicon['Peru'].capital
-puts lexicon['Haiti'].capital
-
-loop do
-  input = gets.chomp
-  if input == 'quit'
-    break
-  elsif lexicon.keys.include? input
-    puts lexicon[input].capital
-  else
-    puts 'unbekanntes Land'
+class Lexicon
+  def run
+    filename = 'laender.csv'
+    lexicon = {}
+    CSV.foreach(filename, headers: true) do |row|
+      # Code,Name,Name (Langform),Hauptstadt,ALPHA-3,TLD,Waehrung,Waehrungscode
+      arr = row # .split(',')
+      c = Country.new(arr[0])
+      c.set_capital(arr[3])
+      c.set_name(arr[1])
+      lexicon[arr[1]] = c
+    end
+    # puts lexicon['Peru'].capital
+    # puts lexicon['Haiti'].capital
+    loop do
+      input = gets.chomp
+      if input == 'quit'
+        break
+      elsif lexicon.keys.include? input
+        puts lexicon[input].capital
+      else
+        puts 'unbekanntes Land'
+      end
+    end
   end
 end
+
+l = Lexicon.new
+l.run
