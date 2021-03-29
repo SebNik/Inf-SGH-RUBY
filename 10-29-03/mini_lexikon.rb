5f0ac8c14e0ee0a0e9bf27a8331dfe2754996525
SebNik <lord.voldmeort1492@gmail.com>
2021-03-29 14:53:09 +0200

    reading in csv files

--- 10-29-03/mini_lexikon.rb
+++ 10-29-03/mini_lexikon.rb
@@ -1,16 +1,18 @@
 # mini_lexikon.rb: This is a lexicon which can access countries.
 #
 # Copyright (C) 2021 Niklas Abraham
+require 'csv'
 require_relative 'Country'
 
 filename = 'laender.csv'
-data = File.readlines(filename, chomp: true)
-# Code,Name,Name (Langform),Hauptstadt,ALPHA-3,TLD,Waehrung,Waehrungscode
+# data = File.readlines(filename, chomp: true)
+# # Code,Name,Name (Langform),Hauptstadt,ALPHA-3,TLD,Waehrung,Waehrungscode
 
 lexicon = {}
 
-data.each do |row|
-  arr = row.split(',')
+# data.each do |row|
+CSV.foreach(filename, headers: true) do |row|
+  arr = row # .split(',')
   c = Country.new(arr[0])
   c.set_capital(arr[3])
   c.set_name(arr[1])
