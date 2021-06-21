eecb3b1b02064abdc04a1636c1c08b0bd395510e
SebNik <lord.voldemort1492@gmail.com>
2021-06-21 13:19:18 +0000

    easier with rand

--- 13-21-06-Algorithmen/shuffle.rb
+++ 13-21-06-Algorithmen/shuffle.rb
@@ -3,7 +3,7 @@ array_start = [*1..10]
 
 new_array = []
 1.upto(array_start.length) do |i|
-    index = rand(0...array_start.length)
+    index = rand(array_start.length)
     new_array << array_start[index]
     array_start.delete_at(index)
 end
