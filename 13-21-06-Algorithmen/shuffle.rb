137bfce5a556642cf049faa86fcfe3d5388d31b1
SebNik <lord.voldemort1492@gmail.com>
2021-06-21 13:18:47 +0000

    added shuffle file

---
+++ 13-21-06-Algorithmen/shuffle.rb
@@ -0,0 +1,10 @@
+array_start = [*1..10]
+# puts array_start[0]
+
+new_array = []
+1.upto(array_start.length) do |i|
+    index = rand(0...array_start.length)
+    new_array << array_start[index]
+    array_start.delete_at(index)
+end
+p new_array
